30cbf8bb941a0faf853b4b7703f3955cf9083502
Shubham Mishra <shubhammishra78640@gmail.com>
2022-02-18 11:33:51 +0530

    updated one

--- src/PortfolioContainer/Projects/Project.jsx
+++ src/PortfolioContainer/Projects/Project.jsx
@@ -18,7 +18,7 @@ export const Project = () => {
                 <div className="projectCardDiv secProject">
                     <div className="projectDetail">
                         <h3>intern Theroy Clone</h3>
-                        <p>InternTheory is an online internship portal created to bridge the gap between students seeking internships and companies looking for interns.</p>
+                        <p>This is a team project where we worked on cloning the Intern Theory website. This website is helful in solving the internship problem between both companies and students by connecting them in a effective manner. We have built this using HTML, CSS, JavaScript and Express JS.</p>
                         <div className="links">
                         <button id="project-links" class="social-media-icon">      <a target="_blank" rel="noopener noreferrer" href="https://github.com/78640sam/intern_theory.github.io.git"><FaGithubSquare className='social-media-icon' /></a>Show Code </button> 
                         <button id="project-links" class="social-media-icon">    <a target="_blank" rel="noopener noreferrer" href="https://intern-theory-clone.vercel.app/"><FaLink className='social-media-icon' /></a>Show live </button> 
@@ -42,7 +42,7 @@ export const Project = () => {
                 <div className="projectCardDiv">
                     <div className="projectDetail">
                         <h3>Juicy Chemistry Clone</h3>
-                        <p>100% organic skin care products in india certified by Ecocert, Order online now in India for your best skin care and experience the proven results.</p>
+                        <p>100% organic skin care products in india certified by Ecocert, Order online now in India for your best skin care and experience the proven results. Juicy Chemistry was born of Pritesh and Megha Asher’s relentless ability to question everything they knew about the world of beauty and personal care. </p>
                         <div className="links">
                         <button id="project-links" class="social-media-icon">    <a target="_blank" rel="noopener noreferrer" href="https://github.com/78640sam/jc.github.io.git"><FaGithubSquare className='social-media-icon' /></a>Show Code </button> 
                         <button id="project-links" class="social-media-icon">    <a target="_blank" rel="noopener noreferrer" href="https://78640sam.github.io/jc.github.io/"><FaLink className='social-media-icon' /></a> Show live </button> 
@@ -69,7 +69,7 @@ export const Project = () => {
                 <div className="projectCardDiv secProject">
                     <div className="projectDetail">
                         <h3>Expedia Clone</h3>
-                        <p>A web application to Check out Expedia's daily travel deals & promotions.</p>
+                        <p>A web application to Check out Expedia's daily travel deals & promotions. A web application to Check out Expedia's daily travel deals & promotions. Expedia is one of the world's leading full-service online travel brands helping travelers easily plan and book their whole trip with the widest selection of vacation packages, flights, hotels, vacation rentals, rental cars, cruises, activities, attractions, and services.</p>
                         <div className="links">
                          <button id="project-links" class="social-media-icon">  <a target="_blank" rel="noopener noreferrer" href="https://github.com/78640sam/expedia.github.io.git"><FaGithubSquare className='social-media-icon' /> </a> Show Code </button> 
                          <button id="project-links" class="social-media-icon">   <a target="_blank" rel="noopener noreferrer" href="facebook.com"><FaLink className='social-media-icon' /></a>Show live </button> 
@@ -93,30 +93,7 @@ export const Project = () => {
             
 
 
-                {/* <div className="projectCardDiv secProject">
-                    <div className="projectDetail">
-                        <h3>Youtube clone</h3>
-                        <p>Youtube is the 2nd largest search engine next to Google.
-                People upload more than 100 hours of videos per minute to YouTube.
-                        </p>
-                        <div className="links">
-                        <button id="project-links" class="social-media-icon">    <a target="_blank" rel="noopener noreferrer" href="https://github.com/78640sam/youtube.github.io.git"><FaGithubSquare className='social-media-icon' /></a>Show Code </button> 
-                        <button id="project-links" class="social-media-icon">    <a target="_blank" rel="noopener noreferrer" href="facebook.com"><FaLink className='social-media-icon' /></a>Show live </button> 
-                        </div>
-                        <div className="langDiv">
-                            <p className="techstack">Tech Stack :</p>
-                            <div className="texttackDiv">
-                            <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="" />
-                                <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="" />
-                                <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="" />
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="projectImg">
-                        <img src=".././images/youtube.jpg" alt="" />
-                    </div>
-                </div> */}
+              
             </div>
         </>
     );
