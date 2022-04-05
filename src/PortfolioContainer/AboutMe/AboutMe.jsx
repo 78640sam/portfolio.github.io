3f0afdc5cd5964c96911b7ce3602758c41c8dccb
Shubham Mishra <shubhammishra78640@gmail.com>
2022-04-06 01:52:35 +0530

    one more update

--- src/PortfolioContainer/AboutMe/AboutMe.jsx
+++ src/PortfolioContainer/AboutMe/AboutMe.jsx
@@ -31,7 +31,7 @@ and trained in Masai school with diverse set of skills ranging from HTML, CSS, J
                     </div>
                     <div className='aboutBtnDiv'>
                         {/* <button className='hireMeBtn'>Hire Me</button> */}
-                        <a href="https://drive.google.com/file/d/11hzUT9o-QSWQ37u6s2elhnrtL6f0ekMM/view?usp=sharing" download='resume.pdf' target="_blank" rel="noopener noreferrer">
+                        <a href=".././images/resume.pdf" download='resume.pdf' target="_blank" rel="noopener noreferrer">
                             <button className="getBtn">Get Resume</button>
                         </a>
                     </div>
