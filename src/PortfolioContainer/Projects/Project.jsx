e17e9e9256848768444d1850927adfdc3d8ece45
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-10 10:34:04 +0530

    updated

--- src/PortfolioContainer/Projects/Project.jsx
+++ src/PortfolioContainer/Projects/Project.jsx
@@ -49,7 +49,7 @@ export const Project = () => {
                         <div className="langDiv">
                             <p className="techstack">Tech Stack :</p>
                             <div className="texttackDiv">
-                            <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="" />
+                                <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="" />
                                 <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="" />
                                 <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="" />
                                 <img src="" alt="" />
