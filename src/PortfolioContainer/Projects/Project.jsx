72ee379dc6ee5a1af1623b819b576592064a121c
Shubham Mishra <shubhammishra78640@gmail.com>
2022-05-10 16:35:17 +0530

    UPDATED

--- src/PortfolioContainer/Projects/Project.jsx
+++ src/PortfolioContainer/Projects/Project.jsx
@@ -28,7 +28,7 @@ export const Project = () => {
                             <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" alt="" />
                                 <img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="" />
                                 <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="" />
-                                <img src="" alt="" />
+                                <img src="https://img.shields.io/badge/React-%231572B6.svg?style=for-the-badge&logo=react&logoColor=white" alt="" />
 
                             </div>
                         </div>
